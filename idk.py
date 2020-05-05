b068f7f8deb2690fe73b39791846f58474a3369d
Greggy2645 <64741164+Greggy2645@users.noreply.github.com>
2020-05-05 15:50:55 +0200

    Update idk.py

--- idk.py
+++ idk.py
@@ -1,6 +1,18 @@
 from random import randint
-begin = input('aight we gonna play a guess the number game. \n If you guesss the number right then you get a balloon. Type start to begin')
+begin = input('aight we gonna play a guess the number game. \n If you guesss the number right\n then you get a balloon. \nType start to begin.\n')
 
-chosenNumber = int(input('kies een nummer:\n'))
-a = randint(0, chosenNumber)
-#to be continued
+def start():
+	if begin == 'start':
+		chosenNumber()
+	else:
+		print('you retard that isnt start')
+def chosenNumber():
+	chosenNumber = int(input('choose a number:\n'))
+	a = randint(0, chosenNumber)
+	guess = int(input('guess the random number here:\n'))
+	if a == guess:
+		print('you did it! A was:' + a )
+	else: 
+		print('helaas pindakaas.\n Jij dacht: ' + str(guess) + 'maar het was: ' + str(a))
+
+start()

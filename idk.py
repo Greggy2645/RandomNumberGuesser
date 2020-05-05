856d03aed1253797557113faadfaf2f336abe062
Greggy2645 <64741164+Greggy2645@users.noreply.github.com>
2020-05-05 14:21:32 +0200

    Create idk.py
    began with this little project.

---
+++ idk.py
@@ -0,0 +1,6 @@
+from random import randint
+begin = input('aight we gonna play a guess the number game. \n If you guesss the number right then you get a balloon. Type start to begin')
+
+chosenNumber = int(input('kies een nummer:\n'))
+a = randint(0, chosenNumber)
+#to be continued
